6574171ddcb60734a2dbbd12a85259d42cd7022f
Palash Jyoti Goswami <palashjyotigoswamipt@gmail.com>
2022-07-21 15:00:37 +0530

    navigation drawer adjusted

--- responsive_app/lib/constants.dart
+++ responsive_app/lib/constants.dart
@@ -8,27 +8,32 @@ var myAppBar = AppBar(
 
 var myDrawer = Drawer(
   backgroundColor: Colors.grey[300],
-  child: Column(
-    children: const [
-      DrawerHeader(
-        child: Icon(Icons.favorite),
-      ),
-      ListTile(
-        leading: Icon(Icons.home),
-        title: Text('D A S H B O A R D'),
-      ),
-      ListTile(
-        leading: Icon(Icons.chat),
-        title: Text('M E S S A G E'),
-      ),
-      ListTile(
-        leading: Icon(Icons.settings),
-        title: Text('S E T T I N G S'),
-      ),
-      ListTile(
-        leading: Icon(Icons.logout),
-        title: Text('L O G O U T'),
-      ),
-    ],
+  child: ListView.builder(
+    itemCount: 1,
+    itemBuilder: (context, index) {
+      return Column(
+        children: const [
+          DrawerHeader(
+            child: Icon(Icons.favorite),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('D A S H B O A R D'),
+          ),
+          ListTile(
+            leading: Icon(Icons.chat),
+            title: Text('M E S S A G E'),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('S E T T I N G S'),
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('L O G O U T'),
+          ),
+        ],
+      );
+    },
   ),
 );
